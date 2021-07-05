477c890ab63285e24062bdf347950aa52c37fae2
kururu <rui.kondo@gmail.com>
2021-07-06 08:57:41 +0900

    20210706 rejectとfilter

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,7 +13,10 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $msg = 'show people record.';
-        $result = Person::get();
+        $result = Person::get()->reject(function($person)
+        {
+            return $person->age < 30;
+        });
         $data = [
             'msg' => $msg,
             'data' => $result,
