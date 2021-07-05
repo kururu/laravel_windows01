b3b90cd96931cebd0fa04ace2d92947a737f4793
kururu <rui.kondo@gmail.com>
2021-07-06 08:54:27 +0900

    20210705 Personクラス元に戻す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Person;
+use App\Person;
 use App\Http\Pagination\MyPaginator;
 
 class HelloController extends Controller
