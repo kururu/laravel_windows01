a027218601e0f201a59c66b11da730656a96110e
kururu <rui.kondo@gmail.com>
2021-07-05 17:26:31 +0900

    20210705 ページネーションEloguent利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Person;
 
 class HelloController extends Controller
 {
@@ -12,7 +13,7 @@ class HelloController extends Controller
     {
         $id = $request->query('page');
         $msg = 'show page: ' . $id;
-        $result = DB::table('people')->simplePaginate(3);
+        $result = Person::paginate(3);
 
         $data = [
             'msg' => $msg,
