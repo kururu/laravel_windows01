0f65407dc2c82fb91e5524fa137396a9a94a4449
kururu <rui.kondo@gmail.com>
2021-07-06 10:11:08 +0900

    20210706 レコードNAMEを大文字に

--- storage/framework/views/fc8e6d163ce13250b24112e8a19576ba0f75d85a.php
+++ storage/framework/views/fc8e6d163ce13250b24112e8a19576ba0f75d85a.php
@@ -8,16 +8,16 @@
     </head>
     <body>
         <div class="container pt-3">
-        <h1>Hello/Index</h1>
-        <p><?php echo e($msg); ?></p>
-        <table class="table table-striped" border="0" style="border-collapse: collapse;">
-        <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <tr>
-            <th class="bg-primary table-dark" style="width: 10%"><?php echo e($item->id); ?></th>
-            <td style="width: 90%"><?php echo e($item->name_and_age); ?></td>
-        </tr>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </table>
+            <h1>Hello/Index</h1>
+            <p><?php echo e($msg); ?></p>
+            <table class="table table-striped" border="0" style="border-collapse: collapse;">
+            <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <tr>
+                <th class="bg-primary table-dark" style="width: 10%"><?php echo e($item->id); ?></th>
+                <td style="width: 90%"><?php echo e($item->name_and_age); ?></td>
+            </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </table>
         </div>
     </body>
 
