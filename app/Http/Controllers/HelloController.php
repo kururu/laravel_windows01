66ae83052d8d4726edccded4100f8ee9e75af196
kururu <rui.kondo@gmail.com>
2021-07-06 09:36:47 +0900

    2021706 レコードmap生成01

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,22 +14,20 @@ class HelloController extends Controller
     {
         $msg = 'show people record.';
         $even = Person::get()->filter(function($item)
-        {
-            return $item->id % 2 == 0;
-        });
-        $even2 = Person::get()->filter(function($item)
-        {
-            return $item->age % 2 == 0;
-        });
-        $result = $even->merge($even2);
+            {
+                return $item->id % 2 == 0;
+            });
+        $map = $even->map(function($item, $key)
+            {
+                return $item->id . ':' . $item->name;
+            });
         
         $data = [
-            'msg' => $msg,
-            'data' => $result,
+            'msg' => $map,
+            'data' => $even,
         ];
         return view('hello.index', $data);
     }
-
 
 
 }
