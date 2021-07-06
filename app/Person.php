c90aa8fcbd96b451e422d4a2bd1a79f8abfbbf59
kururu <rui.kondo@gmail.com>
2021-07-06 10:06:56 +0900

    20210706 アクセサメソッド(name+age) 間隔調整

--- app/Person.php
+++ app/Person.php
@@ -2,8 +2,44 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model; 
+use Illuminate\Database\Eloquent\Collection;
 
 class Person extends Model
 {
+    public function newCollection(array $models = [])
+    {
+        return new MyCollection($models);
+    }
 
+    public function getNameAndIdAttribute()
+	{
+	    return $this->name . ' [id=' . $this->id . ']';
+	}
+
+
+	public function getNameAndMailAttribute()
+	{
+	    return $this->name . ' (' . $this->mail . ')';
+	}
+
+
+	public function getNameAndAgeAttribute()
+	{
+	    return $this->name . '(' . $this->age . ')';
+	}
+	public function getAllDataAttribute()
+	{
+	    return $this->name . '(' . $this->age . ')'
+	        . ' [' . $this->mail . ']';
+	}
+}
+
+
+class MyCollection extends Collection
+{
+    public function fields()
+    {
+        $item = $this->first();
+        return array_keys($item->toArray());
+    }
 }
--- storage/framework/views/fc8e6d163ce13250b24112e8a19576ba0f75d85a.php
+++ storage/framework/views/fc8e6d163ce13250b24112e8a19576ba0f75d85a.php
@@ -4,21 +4,21 @@
         <meta charset="utf-8">
         <title>Index</title>
         <link href="/css/app.css"  rel="stylesheet">
+
     </head>
     <body>
+        <div class="container pt-3">
         <h1>Hello/Index</h1>
         <p><?php echo e($msg); ?></p>
         <table class="table table-striped" border="0" style="border-collapse: collapse;">
         <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
         <tr>
             <th class="bg-primary table-dark" style="width: 10%"><?php echo e($item->id); ?></th>
-            <td style="width: 30%"><?php echo e($item->name); ?></td>
-            <td style="width: 30%"><?php echo e($item->mail); ?></td> 
-            <td style="width: 30%"><?php echo e($item->age); ?></td>
+            <td style="width: 90%"><?php echo e($item->name_and_age); ?></td>
         </tr>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
         </table>
-        <hr>
+        </div>
     </body>
 
 </html><?php /**PATH C:\xampp\htdocs\lara01_git\laravel_windows01\resources\views/hello/index.blade.php ENDPATH**/ ?>
