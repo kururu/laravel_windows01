9d93706c7ee2803dc02d72a1a9f9993a5670c2bf
kururu <rui.kondo@gmail.com>
2021-07-06 16:33:08 +0900

    20210705 ワーカを実行、非同期に対応

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,7 +15,8 @@ class HelloController extends Controller
     {
         if ($person != null)
         {
-            MyJob::dispatch($person);
+            //MyJob::dispatch($person);
+            MyJob::dispatch($person)->delay(now()->addMinutes(5));
         }
         $msg = 'show people record.';
         $result = Person::get();
