7451a6e5662b3f35ef6141fd9b2a50fb9eedd6c4
kururu <rui.kondo@gmail.com>
2021-07-06 17:40:59 +0900

    20210706 php artisan event:generate

---
+++ app/Listeners/PersonEventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\PersonEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class PersonEventListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  PersonEvent  $event
+     * @return void
+     */
+    public function handle(PersonEvent $event)
+    {
+        //
+    }
+}
