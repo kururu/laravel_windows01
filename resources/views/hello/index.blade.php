c677c7a00d267c67de3b9aabba087e0322fd2e56
kururu <rui.kondo@gmail.com>
2021-07-07 09:57:08 +0900

    20210707 $data配列の並べ替え01

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -24,11 +24,53 @@
         </div>
         <hr>
         {{$msg}}
-        <table class="table table-striped" style="width: 500px;">
+        
         <?php
 
-        print_r($data);
-/*
+        echo $msg;
+        //print_r($data[0]);
+
+        echo "<br>";
+        echo "要素の数: ".count($data);
+        echo "<br>";
+
+        $data01 = array_msort($data, array('id'=>SORT_DESC));
+
+        //print_r($data01[0]);
+
+        foreach($data01 as $item){
+            echo $item->id;
+            echo $item->all_data;
+            echo "<br>";
+        }
+        echo "<br>";
+
+        //https://www.php.net/manual/ja/function.array-multisort.php
+        function array_msort($array, $cols)
+        {
+            $colarr = array();
+            foreach ($cols as $col => $order) {
+                $colarr[$col] = array();
+                foreach ($array as $k => $row) { $colarr[$col]['_'.$k] = strtolower($row[$col]); }
+            }
+            $eval = 'array_multisort(';
+            foreach ($cols as $col => $order) {
+                $eval .= '$colarr[\''.$col.'\'],'.$order.',';
+            }
+            $eval = substr($eval,0,-1).');';
+            eval($eval);
+            $ret = array();
+            foreach ($colarr as $col => $arr) {
+                foreach ($arr as $k => $v) {
+                    $k = substr($k,1);
+                    if (!isset($ret[$k])) $ret[$k] = $array[$k];
+                    $ret[$k][$col] = $array[$k][$col];
+                }
+            }
+            return $ret;
+
+        }
+        /*
             $json_search = "$data";
             $array_search = json_decode( $json_search,true ) ;
             sort($array_search);
@@ -43,7 +85,16 @@
             echo "<br>";
             */
         ?>
+        <table class="table table-striped" style="width: 500px;">
         @foreach($data as $item)
+        <tr>
+            <th class="bg-primary table-dark" style="width: 10%">{{$item->id}}</th>
+            <td>{{$item->all_data}}</td>
+        </tr>
+        @endforeach
+        </table>
+        <table class="table table-striped pt-3" style="width: 500px;">
+        @foreach($data01 as $item)
         <tr>
             <th class="bg-primary table-dark" style="width: 10%">{{$item->id}}</th>
             <td>{{$item->all_data}}</td>
--- storage/framework/views/fc8e6d163ce13250b24112e8a19576ba0f75d85a.php
+++ storage/framework/views/fc8e6d163ce13250b24112e8a19576ba0f75d85a.php
@@ -25,11 +25,53 @@
         <hr>
         <?php echo e($msg); ?>
 
-        <table class="table table-striped" style="width: 500px;">
+        
         <?php
 
-        print_r($data);
-/*
+        echo $msg;
+        //print_r($data[0]);
+
+        echo "<br>";
+        echo "要素の数: ".count($data);
+        echo "<br>";
+
+        $data01 = array_msort($data, array('id'=>SORT_DESC));
+
+        //print_r($data01[0]);
+
+        foreach($data01 as $item){
+            echo $item->id;
+            echo $item->all_data;
+            echo "<br>";
+        }
+        echo "<br>";
+
+        //https://www.php.net/manual/ja/function.array-multisort.php
+        function array_msort($array, $cols)
+        {
+            $colarr = array();
+            foreach ($cols as $col => $order) {
+                $colarr[$col] = array();
+                foreach ($array as $k => $row) { $colarr[$col]['_'.$k] = strtolower($row[$col]); }
+            }
+            $eval = 'array_multisort(';
+            foreach ($cols as $col => $order) {
+                $eval .= '$colarr[\''.$col.'\'],'.$order.',';
+            }
+            $eval = substr($eval,0,-1).');';
+            eval($eval);
+            $ret = array();
+            foreach ($colarr as $col => $arr) {
+                foreach ($arr as $k => $v) {
+                    $k = substr($k,1);
+                    if (!isset($ret[$k])) $ret[$k] = $array[$k];
+                    $ret[$k][$col] = $array[$k][$col];
+                }
+            }
+            return $ret;
+
+        }
+        /*
             $json_search = "$data";
             $array_search = json_decode( $json_search,true ) ;
             sort($array_search);
@@ -44,7 +86,16 @@
             echo "<br>";
             */
         ?>
+        <table class="table table-striped" style="width: 500px;">
         <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <th class="bg-primary table-dark" style="width: 10%"><?php echo e($item->id); ?></th>
+            <td><?php echo e($item->all_data); ?></td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </table>
+        <table class="table table-striped pt-3" style="width: 500px;">
+        <?php $__currentLoopData = $data01; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
         <tr>
             <th class="bg-primary table-dark" style="width: 10%"><?php echo e($item->id); ?></th>
             <td><?php echo e($item->all_data); ?></td>
