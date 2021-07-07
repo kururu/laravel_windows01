248b01fd69d562308a798a03a9f0a19d2fccfddd
kururu <rui.kondo@gmail.com>
2021-07-07 11:53:34 +0900

    20210707 artisanコマンド、溜まったキューを実行する

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->exec('mycmd');
+        $schedule->command('queue:work --stop-when-empty');
     }
 
     /**
